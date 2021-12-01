2ecc8fd208631aedb6e3b2cedb300343ae0a2380
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2021-12-01 00:31:47 -0500

    Day 1: Nice version

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
-    let input = std::fs::read_to_string("../day01.txt").unwrap();
-    println!("Problem input: {}", input);
+    let input = std::fs::read_to_string("../day01.txt")
+        .unwrap()
+        .lines()
+        .map(|line| line.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    let part_1 = input.windows(2).filter(|xs| xs[0] < xs[1]).count();
+    println!("Part 1: {}", part_1);
+
+    let part_2 = input.windows(4).filter(|xs| xs[0] < xs[3]).count();
+    println!("Part 2: {}", part_2);
 }
