3ed075f1edf192ca89b759d72326abcefb43b458
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2021-12-01 00:54:41 -0500

    Day 1: One-pass and less allocations

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,13 +1,25 @@
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    let input = std::fs::read_to_string("../day01.txt")
-        .unwrap()
+    let mut input = BufReader::new(File::open("../day01.txt").unwrap())
         .lines()
-        .map(|line| line.parse::<u32>().unwrap())
-        .collect::<Vec<_>>();
+        .map(|line| line.unwrap().parse::<u32>().unwrap())
+        .tuple_windows();
 
-    let part_1 = input.windows(2).filter(|xs| xs[0] < xs[1]).count();
+    let (a, b, c, d) = input.next().unwrap();
+    let mut part_1 = if a < b { 1 } else { 0 };
+    part_1 += if b < c { 1 } else { 0 };
+    part_1 += if c < d { 1 } else { 0 };
+
+    let mut part_2 = if a < d { 1 } else { 0 };
+
+    for (a, _, c, d) in input {
+        part_1 += if c < d { 1 } else { 0 };
+        part_2 += if a < d { 1 } else { 0 };
+    }
+
     println!("Part 1: {}", part_1);
-
-    let part_2 = input.windows(4).filter(|xs| xs[0] < xs[3]).count();
     println!("Part 2: {}", part_2);
 }
