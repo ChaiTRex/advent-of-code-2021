95fe486b2acb75aa3b8a4765b932d30d1a4900b5
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2021-12-06 01:03:02 -0500

    Day 6: Stopped constantly rotating the array

--- rust/day06/src/main.rs
+++ rust/day06/src/main.rs
@@ -1,24 +1,26 @@
 fn main() {
     let mut fish = [0u64; 9];
-    std::fs::read_to_string("../day06.txt").unwrap()
+    std::fs::read_to_string("../day06.txt")
+        .unwrap()
         .split(',')
         .map(|n| n.parse::<usize>().unwrap())
         .for_each(|n| fish[n] += 1);
-    
-    (0..80).for_each(|_| after_one_day(&mut fish));
-    let part_1 = fish.iter().sum::<u64>();
-    println!("Part 1: {}", part_1);
-    
-    (80..256).for_each(|_| after_one_day(&mut fish));
-    let part_2 = fish.iter().sum::<u64>();
-    println!("Part 2: {}", part_2);
-}
 
-fn after_one_day(fish: &mut [u64]) {
-    let reproducing_fish = fish[0];
-    for i in 0..8 {
-        fish[i] = fish[i + 1];
-    }
-    fish[6] += reproducing_fish;
-    fish[8] = reproducing_fish;
+    let mut day_cycle = (7..=8).chain(0..7).zip(0..=8).cycle();
+
+    let part_1 = {
+        for (i, j) in day_cycle.by_ref().take(80) {
+            fish[i] += fish[j];
+        }
+        fish.iter().sum::<u64>()
+    };
+    println!("Part 1: {}", part_1);
+
+    let part_2 = {
+        for (i, j) in day_cycle.take(256 - 80) {
+            fish[i] += fish[j];
+        }
+        fish.iter().sum::<u64>()
+    };
+    println!("Part 2: {}", part_2);
 }
