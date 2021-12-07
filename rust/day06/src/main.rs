8b5454b798bb15da31d2a4f2e524444a482af328
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2021-12-06 23:25:38 -0500

    Day 6: Linear algebra simplification

--- rust/day06/src/main.rs
+++ rust/day06/src/main.rs
@@ -1,26 +1,34 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY_80_AND_256_WEIGHTS: [(u64, u64); 5] = [
+    (1401, 6206821033),
+    (1191, 5617089148),
+    (1154, 5217223242),
+    (1034, 4726100874),
+    (950, 4368232009),
+];
+
 fn main() {
-    let mut fish = [0u64; 9];
-    std::fs::read_to_string("../day06.txt")
-        .unwrap()
-        .split(',')
-        .map(|n| n.parse::<usize>().unwrap())
-        .for_each(|n| fish[n] += 1);
+    let mut part_1 = 0;
+    let mut part_2 = 0;
 
-    let mut day_cycle = (7..=8).chain(0..7).zip(0..=8).cycle();
-
-    let part_1 = {
-        for (i, j) in day_cycle.by_ref().take(80) {
-            fish[i] += fish[j];
+    let mut input = BufReader::new(File::open("../day06.txt").unwrap());
+    let mut read_buffer = Vec::with_capacity(2);
+    loop {
+        match input.read_until(b',', &mut read_buffer) {
+            Ok(0) => break,
+            Ok(1 | 2) => {
+                let (part_1_weight, part_2_weight) =
+                    DAY_80_AND_256_WEIGHTS[(read_buffer[0] - b'1') as usize];
+                part_1 += part_1_weight;
+                part_2 += part_2_weight;
+                read_buffer.clear();
+            }
+            _ => panic!("Invalid input"),
         }
-        fish.iter().sum::<u64>()
-    };
+    }
+
     println!("Part 1: {}", part_1);
-
-    let part_2 = {
-        for (i, j) in day_cycle.take(256 - 80) {
-            fish[i] += fish[j];
-        }
-        fish.iter().sum::<u64>()
-    };
     println!("Part 2: {}", part_2);
 }
