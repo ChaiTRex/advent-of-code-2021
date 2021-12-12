2369be5af24c1d58f64fa77fec5f4fc46cb74d96
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2021-12-12 01:23:54 -0500

    Day 11: Initial solution

--- rust/day11/src/main.rs
+++ rust/day11/src/main.rs
@@ -1,4 +1,86 @@
 fn main() {
-    let input = std::fs::read_to_string("../day11.txt").unwrap();
-    println!("Problem input: {}", input);
+    let mut octopuses = [[0; 10]; 10];
+    std::fs::read_to_string("../day11.txt").unwrap()
+        .lines()
+        .zip(0..)
+        .for_each(|(line, y)| {
+            line.bytes()
+                .zip(0..)
+                .for_each(|(ch, x)| {
+                    octopuses[y][x] = ch - b'0';
+                });
+        });
+    
+    let mut flash_count = 0;
+    let mut time_step = 0;
+    
+    let mut part_2_complete = false;
+    
+    while time_step < 100 || !part_2_complete {
+        time_step += 1;
+        let mut already_flashed = [[false; 10]; 10];
+        
+        // Initial increase by 1
+        for y in 0..10 {
+            for x in 0..10 {
+                octopuses[y][x] += 1;
+            }
+        }
+        
+        let mut new_flashes_happened = true;
+        while new_flashes_happened {
+            new_flashes_happened = false;
+            
+            for y in 0..10 {
+                for x in 0..10 {
+                    if octopuses[y][x] > 9 && !already_flashed[y][x] {
+                        new_flashes_happened = true;
+                        flash_count += 1;
+                        already_flashed[y][x] = true;
+                        
+                        if y != 0 {
+                            if x != 0 {
+                                octopuses[y - 1][x - 1] += 1;
+                            }
+                            octopuses[y - 1][x] += 1;
+                            if x != 9 {
+                                octopuses[y - 1][x + 1] += 1;
+                            }
+                        }
+                        if x != 0 {
+                            octopuses[y][x - 1] += 1;
+                        }
+                        if x != 9 {
+                            octopuses[y][x + 1] += 1;
+                        }
+                        if y != 9 {
+                            if x != 0 {
+                                octopuses[y + 1][x - 1] += 1;
+                            }
+                            octopuses[y + 1][x] += 1;
+                            if x != 9 {
+                                octopuses[y + 1][x + 1] += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+            // Set flashed octopuses to energy level zero
+        for y in 0..10 {
+            for x in 0..10 {
+                if already_flashed[y][x] {
+                    octopuses[y][x] = 0;
+                }
+            }
+        }
+        if time_step == 100 {
+            println!("Part 1: {}", flash_count);
+        }
+        
+        if already_flashed.iter().all(|row| row.iter().all(|&value| value)) {
+            println!("Part 2: {}", time_step);
+            part_2_complete = true;
+        }
+    }
 }
