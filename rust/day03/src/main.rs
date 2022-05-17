1880eab83903e903cbdf954c62e92c9313043a01
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-05-16 20:52:40 -0400

    Day 3: Improved file parsing memory usage, computations use bitwise operations

--- rust/day03/src/main.rs
+++ rust/day03/src/main.rs
@@ -1,74 +1,82 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    let input = std::fs::read_to_string("../day03.txt").unwrap()
-        .lines()
-        .map(|line| line.to_owned())
-        .collect::<Vec<_>>();
-    println!("Problem input: {:?}", input);
-    
-    let mut line_count = 0;
-    let mut one_bit_counts = vec![0; 12];
-    
-    for line in input.iter() {
-        line_count += 1;
-        for (i, bit) in (0usize .. 12).zip(line.chars()) {
-            one_bit_counts[i] += if bit == '1' { 1 } else { 0 };
+    let input = {
+        let mut input = Vec::with_capacity(1000);
+
+        let file = match File::open("../day03.txt") {
+            Ok(file) => file,
+            Err(e) => panic!("Error reading file: {:?}", e),
+        };
+        let mut file = BufReader::new(file);
+        let mut line = String::with_capacity(14);
+
+        loop {
+            line.clear();
+            match file.read_line(&mut line) {
+                Ok(0) => break,
+                Ok(_) => match u16::from_str_radix(line.trim_end(), 2) {
+                    Ok(n) => input.push(n),
+                    Err(e) => panic!("Error reading file: {:?}", e),
+                },
+                Err(e) => panic!("Error reading file: {:?}", e),
+            }
+        }
+
+        input
+    };
+
+    let mut one_bit_counts = [0; 12];
+    for n in input.iter() {
+        for (i, count) in (0..12).rev().zip(one_bit_counts.iter_mut()) {
+            *count += ((n >> i) & 1) as usize;
         }
     }
-    
-    println!("{:?}", one_bit_counts);
-    
-    let mut result = 0u32;
-    
-    for one_bit_count in one_bit_counts {
-        result <<= 1;
-        result |= if one_bit_count > line_count - one_bit_count { 1 } else { 0 };
-    }
-    println!("{}", result);
-    println!("Part 1: {}", result * (!result & 0b111111111111));
-    
-    let mut input_oxy = input.clone();
-    for bit_position in 0 .. 12 {
-        let mut line_count = 0;
-        let mut one_bit_count = 0;
-        
-        for line in input_oxy.iter() {
-            line_count += 1;
-            one_bit_count += if line.as_bytes()[bit_position] == '1' as u8 { 1 } else { 0 };
+
+    let result = one_bit_counts.iter().fold(0, |result, n| {
+        (result << 1) | ((n << 1) > input.len()) as u32
+    });
+
+    println!("Part 1: {}", result * (result ^ 0b1111_1111_1111));
+
+    let oxy = {
+        let mut input = input.clone();
+
+        for bit_position in (0..12).rev() {
+            let one_bit_count = input
+                .iter()
+                .map(|&n| ((n >> bit_position) & 1) as usize)
+                .sum::<usize>();
+            let to_keep = ((one_bit_count << 1) >= input.len()) as u16;
+
+            input.retain(|n| (n >> bit_position) & 1 == to_keep);
+            if input.len() == 1 {
+                break;
+            }
         }
-        
-        let to_keep = if one_bit_count >= line_count - one_bit_count { '1' } else { '0' };
-        input_oxy.retain(|line| line.as_bytes()[bit_position] == to_keep as u8);
-        if input_oxy.len() == 1 { break; }
-    }
-    let mut oxy = 0;
-    let input_oxy = input_oxy.pop().unwrap();
-    for ch in input_oxy.chars() {
-        oxy <<= 1;
-        oxy |= if ch == '1' { 1 } else { 0 };
-    }
-    println!("{}", oxy);
-    
-    let mut input_co2 = input.clone();
-    for bit_position in 0 .. 12 {
-        let mut line_count = 0;
-        let mut one_bit_count = 0;
-        
-        for line in input_co2.iter() {
-            line_count += 1;
-            one_bit_count += if line.as_bytes()[bit_position] == '1' as u8 { 1 } else { 0 };
+
+        input.pop().unwrap() as u32
+    };
+
+    let co2 = {
+        let mut input = input;
+
+        for bit_position in (0..12).rev() {
+            let one_bit_count = input
+                .iter()
+                .map(|&n| ((n >> bit_position) & 1) as usize)
+                .sum::<usize>();
+            let to_keep = ((one_bit_count << 1) < input.len()) as u16;
+
+            input.retain(|n| (n >> bit_position) & 1 == to_keep);
+            if input.len() == 1 {
+                break;
+            }
         }
-        
-        let to_keep = if one_bit_count >= line_count - one_bit_count { '0' } else { '1' };
-        input_co2.retain(|line| line.as_bytes()[bit_position] == to_keep as u8);
-        if input_co2.len() == 1 { break; }
-    }
-    let mut co2 = 0;
-    let input_co2 = input_co2.pop().unwrap();
-    for ch in input_co2.chars() {
-        co2 <<= 1;
-        co2 |= if ch == '1' { 1 } else { 0 };
-    }
-    println!("{}", co2);
-    
+
+        input.pop().unwrap() as u32
+    };
+
     println!("Part 2: {}", oxy * co2);
 }
